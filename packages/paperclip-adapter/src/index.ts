8f80689a41448110e5af22a2c4b148856fac65f2
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2026-05-18 16:45:11 -0300

    feat(adapter): external adapter + OMC transcript parser

--- packages/paperclip-adapter/src/index.ts
+++ packages/paperclip-adapter/src/index.ts
@@ -4,4 +4,19 @@
 
 export { HttpAdapter } from "./http-adapter.js";
 export type { HttpAdapterOptions } from "./http-adapter.js";
-export type { HttpAdapterConfig, RoleId } from "./types.js";
+export { ExternalAdapter } from "./external-adapter.js";
+export type {
+  ExternalAdapterOptions,
+  PollResult,
+  CheckoutResult,
+  CompleteResult,
+} from "./external-adapter.js";
+export { parseTranscript } from "./transcript-parser.js";
+export type {
+  HttpAdapterConfig,
+  RoleId,
+  TranscriptStep,
+  OmcArtifact,
+  RichCommentBlock,
+  AdapterResult,
+} from "./types.js";
